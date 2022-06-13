abebf53cb646984a91aeb895f0105fb68ff2d096
ngovinhhuy <31349426+ngovinhhuy@users.noreply.github.com>
2022-06-13 15:27:20 +0700

    Add files via upload

---
+++ player.cpp
@@ -0,0 +1,42 @@
+#include <ncurses.h>
+#include <time.h>
+#include <stdlib.h>
+char waitframe(time_t start,int fps,int next_frame){
+    while(1){
+        time_t now=time(NULL);
+        if((double)(next_frame/fps)<=(now-start))
+            return 1;
+    }
+}
+int play(char *fname,int set_fps){
+    FILE *f=fopen(fname,"r");
+    if(f==NULL){
+        mvprintw(0,0,"Unale to open file!\n");
+        return 2;
+    }
+    else{
+        initscr();
+        char chunk[162];
+        int framecount=0;
+        time_t time_start=time(NULL);
+        while(fgets(chunk,sizeof(chunk),f)!=NULL){
+            if(chunk[0]=='R'){
+                move(0,0);
+                waitframe(time_start,set_fps,framecount);
+                framecount++;
+            }
+            else{
+                printw(chunk);
+                refresh();
+            }
+        }
+        endwin();  
+        return 0;
+    }         
+}
+int main(int argc, char *argv[]){
+    if(argc !=3)
+        printf("Usage: %s <file_name> <fps>\n",argv[0]);
+    else
+        return play(argv[1],atoi(argv[2]));
+}
---
+++ player_py3.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import curses
+import sys
+import time
+
+def waitFrame(start, fps, next_frame):
+    while True:
+        now = time.perf_counter()
+        # Exit this function delta reaches time to display a next frame.
+        if next_frame / fps <= now-start:
+            return
+def play(window, fname, set_fps):
+    # Initialize start_time
+    start_time = time.perf_counter()
+    # curses.delay_output(0)
+
+    try:
+        f = open(fname)
+        # Initialize counter of played out frames.
+        framecount = 0
+        # Display all lines in order.
+        for line in f:
+            # Reset cursor position after complete to show a frame.
+            if line[0] == 'R':
+                window.move(0, 0)
+                # curses.delay_output(0)
+                # Wait next frame
+                waitFrame(start_time, set_fps, framecount)
+                # Increase frame counter.
+                framecount+=1
+            else:
+                # Output current line to stdout.
+                window.addstr(line)
+                # Refresh window.
+                window.refresh()
+    except curses.error:
+        window.addstr(0, 0, "Windows size is too small")
+        window.addstr(
+            1, 0, "Required Windows Size X, Y: 161, 61 Current: %s, %s" % window.getmaxyx())
+        window.addstr(2, 0, "Expand window size and try again.")
+        window.addstr(3, 0, "Press any key to exit...")
+        window.refresh()
+        window.getch()
+    except:
+        raise
+
+# Entry point of this application.
+# Terminate the application if no argument set.
+def main():
+    if len(sys.argv) < 3:
+        print("No argument.")
+        return
+    fn_list = sys.argv[1]
+    set_fps = int(sys.argv[2])
+    curses.wrapper(play, fn_list, set_fps)
+main()
